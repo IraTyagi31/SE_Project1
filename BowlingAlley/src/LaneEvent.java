fe72383918742728b98ea224193ba150a4fc33c6
Arpit Maheshwari <arpitmaheshwari14@gmail.com>
2022-02-19 18:19:35 +0530

    Refactored LaneEvent.java

--- BowlingAlley/src/LaneEvent.java
+++ BowlingAlley/src/LaneEvent.java
@@ -23,62 +23,38 @@
  *
  */
 
+
+
 import java.util.HashMap;
 
-public class LaneEvent implements LaneEventInterface{
+public class LaneEvent extends LaneEventUtil {
+
+	public boolean mechProb;
+
+	public int[][] cumulScore;
 
 	private Party p;
-	int frame;
-	int ball;
-	Bowler bowler;
-	int[][] cumulScore;
-	HashMap score;
-	int index;
-	int frameNum;
-	int[] curScores;
-	boolean mechProb;
-	
-	public LaneEvent( Party pty, int theIndex, Bowler theBowler, int[][] theCumulScore, HashMap theScore, int theFrameNum, int[] theCurScores, int theBall, boolean mechProblem) {
+	public Bowler bowler;
+	public HashMap score;
+
+
+	public LaneEvent( Party pty, int theIndex, Bowler theBowler, int[][] theCumulScore, HashMap theScore, int theFrameNum, int theBall, boolean mechProblem) {
+		super(theBall, theIndex, theFrameNum);
 		p = pty;
-		index = theIndex;
 		bowler = theBowler;
 		cumulScore = theCumulScore;
 		score = theScore;
-		curScores = theCurScores;
-		frameNum = theFrameNum;
-		ball = theBall;	
 		mechProb = mechProblem;
 	}
 	
 	public boolean isMechanicalProblem() {
 		return mechProb;
 	}
-	
-	public int getFrameNum() {
-		return frameNum;
-	}
-	
+
 	public HashMap getScore( ) {
 		return score;
 	}
 
-
-	public int[] getCurScores(){ 
-		return curScores;
-	}
-	
-	public int getIndex() {
-		return index;
-	}
-
-	public int getFrame( ) {
-		return frame;
-	}
-
-	public int getBall( ) {
-		return ball;
-	}
-	
 	public int[][] getCumulScore(){
 		return cumulScore;
 	}
