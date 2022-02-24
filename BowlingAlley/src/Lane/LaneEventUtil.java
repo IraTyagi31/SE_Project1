fc7cea0b2dd261df06d5067430668318c8804fff
IraTyagi31 <89247356+IraTyagi31@users.noreply.github.com>
2022-02-24 12:33:24 +0530

    Final Refactored Code

---
+++ BowlingAlley/src/Lane/LaneEventUtil.java
@@ -0,0 +1,36 @@
+package Lane;
+
+//import Lane.*;
+//import Main.*;
+//import ControlDesk.*;
+//import PinSetter.*;
+//import Score.*;
+
+public class LaneEventUtil {
+    public int frame;
+    public int ball;
+    public int index;
+    public int frameNum;
+
+    public LaneEventUtil(int theBall, int theIndex, int theFrameNum) {
+        ball = theBall;
+        index = theIndex;
+        frameNum = theFrameNum;
+    }
+
+    public int getFrameNum() {
+        return frameNum;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getFrame( ) {
+        return frame;
+    }
+
+    public int getBall( ) {
+        return ball;
+    }
+}
